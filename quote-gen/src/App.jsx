8b20826312768270d113f54104eef7d134339d5e
Corey le <endergreen4@gmail.com>
2025-05-11 19:23:45 +1000

    first commit

---
+++ quote-gen/src/App.jsx
@@ -0,0 +1,13 @@
+import QuoteGenerator from './components/Quotegenerator'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <QuoteGenerator/>
+    </>
+  )
+}
+
+export default App
---
+++ quote-gen/src/components/Quotegenerator.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react"
+
+
+
+export default function QuoteGenerator(){
+
+    const quotes = [
+    "You can't use up creativity. The more you use, the more you have. - Maya Angelodu",
+    "The best way to predict the future is to create it. - Peter Drucker",
+    "Let us pick up our books and our pens, they are the most powerful weapons. - Malala Yousafzai",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "It always seems impossible until it’s done. - Nelson Mandela",
+    "I am not afraid... I was born to do this. - Joan of Arc",
+    "Believe you can and you’re halfway there. - Theodore Roosevelt"
+    ];
+    const [currentQuote, setCurrentQuote] = useState(quotes[0])
+        
+    return(
+        <>
+        <h1 className="quoteClass">{currentQuote}</h1>
+        <button className="quoteButton">Generate Random Quote</button>
+        </>
+    )
+}
